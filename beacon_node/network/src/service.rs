9e23b14621d65136ecea2b50a0ceafb70372308a
Age Manning <Age@AgeManning.com>
2025-05-12 15:23:29 +1000

    Mallory - Single commit

--- beacon_node/network/src/service.rs
+++ beacon_node/network/src/service.rs
@@ -260,6 +260,8 @@ impl<T: BeaconChainTypes> NetworkService<T> {
 
         debug!(fork_name = ?fork_context.current_fork(), "Current fork");
 
+        let keypair = lighthouse_network::load_private_key(&config);
+
         // construct the libp2p service context
         let service_context = Context {
             config: config.clone(),
@@ -267,10 +269,13 @@ impl<T: BeaconChainTypes> NetworkService<T> {
             fork_context: fork_context.clone(),
             chain_spec: beacon_chain.spec.clone(),
             libp2p_registry,
+            keypair,
+            incoming_connections: None,
         };
 
         // launch libp2p service
-        let (mut libp2p, network_globals) = Network::new(executor.clone(), service_context).await?;
+        let (mut libp2p, network_globals) =
+            Network::new(executor.clone(), service_context, None).await?;
 
         // Repopulate the DHT with stored ENR's if discovery is not disabled.
         if !config.disable_discovery {
@@ -476,6 +481,11 @@ impl<T: BeaconChainTypes> NetworkService<T> {
         shutdown_sender: &mut Sender<ShutdownReason>,
     ) {
         match ev {
+            // mallory event
+            NetworkEvent::MallorySwarmEvent(_) => {}
+            // mallory event
+            NetworkEvent::IdentifyReceived(_) => {}
+
             NetworkEvent::PeerConnectedOutgoing(peer_id) => {
                 self.send_to_router(RouterMessage::StatusPeer(peer_id));
             }
